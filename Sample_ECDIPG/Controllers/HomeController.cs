ba55bd804c703918c725bc433ac60062bfeeae96
magid021 <magid021@gmail.com>
2018-10-31 11:57:04 +0330

    init

---
+++ Sample_ECDIPG/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample_ECDIPG.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+         
+
+        [HttpPost]
+        public JsonResult SendCustomerToIPG(long Amount, string Language, string BuyID)
+        {
+            DateTime DateNow = DateTime.Now;
+
+            Models.IPGECD IPGECD = new Models.IPGECD();
+         
+            Models.IPGECD.IPGResult Result = IPGECD.PayRequest("Test", "96100000", BuyID, Amount, DateNow.ToString("yyyy/MM/dd"), DateNow.ToString("HH:mm"), "http://localhost:42202/Home/ResiveCustomerFromIPG", Language);
+
+            return Json(Result);
+           
+        }
+
+        //آدرس برگشت
+        [HttpPost]
+        public ActionResult ResiveCustomerFromIPG(Models.IPGECD.PayStartResult Result)
+        {
+            Models.IPGECD.IPGResult Model_Result = new Models.IPGECD.IPGResult();
+
+            Models.IPGECD IPGECD = new Models.IPGECD();
+
+
+            
+            long Customer_Amount=10000;
+
+            try
+            {
+                
+                if (Result.State != 1)
+                {
+
+                    ViewBag.Error = Result.ErrorCode + " - " + Result.ErrorDescription;
+                    return View("ErrorPage");
+                }
+               
+
+                //در این قسمت با استفاده از شناسه خرید میتوانید خرید مشتری را یافت کنید و سپس مبلغ مشتری  با مبلغ تراکنش چک می شود اگر مبلغ مشتری با مبلغ تراکنش یکسان نباشد متد برگشت وجه صدا زده میشود
+            if(Result.Amount !=Customer_Amount)
+            {
+                IPGECD.PayReverse(Result.Token);
+
+                 ViewBag.Error = "مبلغ کسر شده در تراکنش با مبلغ مشتری یکسان نمی باشد";
+                    return View("ErrorPage");
+            }
+
+            
+              Models.IPGECD.IPGResult Model_ResultConf=IPGECD.PayConfirmation(Result.Token);
+
+              if (Model_ResultConf.State != 1)
+                   {
+                       ViewBag.Error = Model_Result.ErrorCode + " - " + Model_Result.ErrorDescription;
+                       return View("ErrorPage");
+                   }
+
+                   return View("SuccessTransaction");
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View("ErrorPage");
+               
+            }
+
+           
+        }
+
+    }
+}
---
+++ Sample_ECDIPG/Models/IPGECD.cs
@@ -0,0 +1,165 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace Sample_ECDIPG.Models
+{
+    public class IPGECD
+    {
+
+
+        public  const string PayStart_url = "https://ecd.shaparak.ir/ipg_ecd/PayStart";
+        private const string PayRequest_url = "https://ecd.shaparak.ir/ipg_ecd/PayRequest";
+        private const string PayConfirmation_url = "https://ecd.shaparak.ir/ipg_ecd/PayConfirmation";
+        private const string PayReverse_url = "https://ecd.shaparak.ir/ipg_ecd/PayReverse";
+        
+
+        public IPGResult PayRequest(string Key, string TerminalNumber, string BuyID, long Amount, string Date, string Time, string RedirectURL, string Language)
+        {
+            IPGResult Result = new IPGResult();
+            try
+            {
+
+                string Checksum = HashSHa1(TerminalNumber + BuyID + Amount.ToString() + Date + Time + RedirectURL + Key);
+
+                var model_Send = new { TerminalNumber = TerminalNumber, BuyID = BuyID, Amount = Amount, Date = Date, Time = Time, RedirectURL = RedirectURL, Language = Language, CheckSum = Checksum };
+
+
+
+                var webClient = new WebClient();
+
+                webClient.Encoding = System.Text.Encoding.UTF8;
+                webClient.Headers[HttpRequestHeader.ContentType] = "application/json;charset=utf-8";
+                var rawMessage = JsonConvert.SerializeObject(model_Send);
+
+                rawMessage = webClient.UploadString(PayRequest_url, rawMessage);
+
+                Result = JsonConvert.DeserializeObject<IPGResult>(rawMessage);
+            
+                return Result;
+            }
+            catch (Exception)
+            {
+
+                return Result;
+            }
+        }
+
+
+        public IPGResult PayConfirmation(string Token)
+        {
+            IPGResult Result = new IPGResult();
+
+            try
+            {
+
+                var model_Send = new { Token = Token };
+
+                var webClient = new WebClient();
+
+                webClient.Encoding = System.Text.Encoding.UTF8;
+                webClient.Headers[HttpRequestHeader.ContentType] = "application/json;charset=utf-8";
+                var rawMessage = JsonConvert.SerializeObject(model_Send);
+
+                rawMessage = webClient.UploadString(PayConfirmation_url, rawMessage);
+
+                Result = JsonConvert.DeserializeObject<IPGResult>(rawMessage);
+
+
+                return Result;
+            }
+            catch (Exception)
+            {
+
+                return Result;
+            }
+        }
+
+
+        public IPGResult PayReverse(string Token)
+        {
+            IPGResult Result = new IPGResult();
+
+            try
+            {
+              
+                var model_Send = new { Token = Token };
+
+                var webClient = new WebClient();
+                webClient.Encoding = System.Text.Encoding.UTF8;
+                webClient.Headers[HttpRequestHeader.ContentType] = "application/json;charset=utf-8";
+                var rawMessage = JsonConvert.SerializeObject(model_Send);
+                rawMessage = webClient.UploadString(PayReverse_url, rawMessage);
+                Result = JsonConvert.DeserializeObject<IPGResult>(rawMessage);
+
+                return Result;
+            }
+            catch (Exception)
+            {
+
+                return Result;
+            }
+        }
+
+
+
+        #region Public Class
+        public class IPGResult
+        {
+            public IPGResult()
+            {
+                this.ErrorCode = string.Empty;
+                this.ErrorDescription = string.Empty;
+                this.Res = string.Empty;
+                this.State = 0;
+            }
+
+            public int State { get; set; }
+            public string Res { get; set; }
+            public string ErrorDescription { get; set; }
+            public string ErrorCode { get; set; }
+        }
+
+        public class PayStartResult
+        {
+           
+
+            public string Token { get; set; }
+            public int State { get; set; }
+            public string ErrorCode { get; set; }
+            public string BuyID { get; set; }
+            public string TrackingNumber { get; set; }
+            public string ReferenceNumber { get; set; }
+            public string ErrorDescription { get; set; }
+            public long Amount { get; set; }
+        }
+
+        #endregion Public Class
+
+        #region Private Function
+        private string HashSHa1(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        #endregion Private Function
+
+    }
+}
